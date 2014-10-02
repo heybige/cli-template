26581319e4d25301016ea12502e04dbab87f7c7c
heybige <eric@heybige.com>
2014-10-02 09:36:36 -0700

    initial commit

---
+++ tests/TemplateTest.php
@@ -0,0 +1,11 @@
+<?php
+	require_once("../src/lib/template.php");
+
+	class TemplateTest extends PHPUnit_Framework_TestCase {
+
+	    public function testTemplate() {
+			$this->assertTrue(true);
+		}
+
+	}
+?>
